ff79fc09734709114227b491435c0cd588bd7476
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-04-18 17:16:33 +0600

    Fix build

--- build-dist.sh
+++ build-dist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm -rf dist/*
+rm -rf dist/* reverso.zip
 mkdir -p dist/{css,img,js}
 cp -r js dist
 cp -r img dist
 cp css/{ani*,owl*,styles.css} dist/css
+cp index.html dist
 zip -r reverso.zip dist
